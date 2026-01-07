0c7ad113edf2b09d2d7139685e5387daafdca0f8
Entzyyy <ishak.bouk07@outlook.com>
2026-01-07 17:10:46 +0100

    basics

--- Optioun/main.py
+++ Optioun/main.py
@@ -1,14 +1,13 @@
 # Ishak Boukellal – Nutrition App (POC)
 
 import streamlit as st
+from PIL import Image
 
-# -----------------------------
-# Seiten / Funktionen
-# -----------------------------
+st.set_page_config(page_title="Nutrition App", page_icon="🥗", layout="centered")
 
 def scanner():
     st.header("📷 Scanner")
-    item = st.text_input("Lebensmittel eingeben (Mock-Scan)")
+    item = st.text_input("Lebensmittel eingeben (Scan)")
     if st.button("Scannen"):
         if item:
             st.success(f"{item} wurde analysiert ✅")
@@ -26,14 +25,22 @@ def trends():
 
     st.subheader("🍕 Protein Pizza")
     st.write("Trend auf TikTok – besser als klassische Pizza.")
-
+ 
 
 def stores():
     st.header("🛒 Stores")
 
     st.write("Empfohlene Produkte:")
-    st.write("• Migros – Bio Haferflocken")
-    st.write("• Coop – Protein Joghurt")
+    st.write("• Cactus - Bio Haferflocken")
+    st.write("• Lidl   - Protein Joghurt")
+
+
+    products = [
+        {"name": "Migros – Bio Haferflocken", "key": "migros"},
+        {"name": "Coop – Protein Joghurt", "key": "coop"},
+        {"name": "Aldi – Mandelmilch", "key": "aldi"},
+    ]
+
 
 
 def community():
@@ -44,3 +51,39 @@ def community():
 def profile():
     st.header("👤 Profil")
     st.write("Name: Ishak")
+    if st.button("Abmelden"):
+        pass
+
+def fridge():
+    st.header("🧊 Kühlschrank (coming soon)")
+    st.info("Hier kannst du demnächst deine Vorräte verwalten.")
+
+
+
+PAGES = {
+    "📷 Scanner": scanner,
+    "🧊 Kühlschrank": fridge,
+    "🔥 Trends & Rezepte": trends,
+    "🛒 Stores": stores,
+    "💬 Community": community,
+    "👤 Profil": profile, }
+
+
+st.sidebar.title("𝙸𝚜𝚑𝚊𝚔")
+
+page = st.sidebar.radio("Seite auswählen", ["Scanner🔍", "Kühlschrank 🧊", "Trends & Rezepte 🍱", "Stores🏪", "Community🫂", "Profil😀"])
+
+if page == "Scanner🔍":
+    scanner()
+elif page == "Trends & Rezepte 🍱":
+    trends()
+elif page == "Stores🏪":
+    stores()
+elif page == "Kühlschrank 🧊":
+    fridge()
+elif page == "Community🫂":
+    community()
+elif page == "Profil😀":
+    profile()
+    
+   
